691499ae6d4d6bcdbadf72a14dc5441d248d19a4
Igor Borovenko <mk_fan@ukr.net>
2022-02-17 18:45:55 +0200

    react-query

--- Hipster.App/src/App.tsx
+++ Hipster.App/src/App.tsx
@@ -2,23 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import clients from "./api";
-import {Book} from "./api.generated";
+import { useQuery } from 'react-query';
 
 function BookList() {
-  const [books, setBooks] = React.useState<Book[]>([]);
-
-  React.useEffect(() => {
-    clients.books.get().then((books) => setBooks(books));
-  }, []);
-
-  if (!books)
+  const booksQuery = useQuery("all-books", () => clients.books.get());
+  
+  if (booksQuery.isLoading)
     return <div>Loading...</div>;
 
+  if (booksQuery.error)
+    return <div>Error {(booksQuery.error as {}).toString()}</div>
+    
   return (
     <div>
       <h1>Books</h1>
 
-      {books.map(book => (
+      {booksQuery.data!.map(book => (
         <div key={book.id}>
           <h2>"{book.title}"</h2>
           <h4>by {book.author}</h4>
